f593f282cd25e6a8528f133c162eb2b7989ac6b2
Opt3dRx <96031147+Opt3dRx@users.noreply.github.com>
2022-11-12 18:56:49 -0500

    finished functionality for one mp4 type

--- flask_app/static/js/user.js
+++ flask_app/static/js/user.js
@@ -4,6 +4,16 @@ function download_video() {
     var data = new FormData();
 
     var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log('should appear here')
+            console.log(this.response)
+            console.log(this.response)
+            
+            document.getElementById('video').href = this.response
+
+        }
+    }
 
     request.responseType = "";
 
