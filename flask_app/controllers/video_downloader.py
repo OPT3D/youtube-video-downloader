efab233166836da910c4fcc66bb0a97977619d90
Opt3dRx <96031147+Opt3dRx@users.noreply.github.com>
2022-11-14 18:03:45 -0500

    fixed some changes

--- flask_app/controllers/video_downloader.py
+++ flask_app/controllers/video_downloader.py
@@ -1,22 +1,49 @@
 # Imports
 
 from flask_app import app
-from flask import render_template,redirect,request,session,flash,jsonify,make_response
+from flask import render_template,redirect,request,session,flash,jsonify,make_response,send_from_directory
 from pytube import YouTube
+import os
 
-@app.route("/", methods = ['GET', 'POST'])
+@app.route("/")
 def video_downloader_home():
-    video_format = ''
-    if request.method == 'POST':
-            print(request.form['url'])
-            
-            yt = YouTube(request.form['url'])
-            print(yt.title)
-            stream = yt.streams.get_by_itag(22)
-            stream.download("flask_app/static/videos")
-            video = f'http://localhost:5000/static/videos/{yt.title}'
-            video_url = f'{video}.mp4'
-            print(video)
-            return video_url, 200
         
-    return render_template("video_downloader_home.html")
+        return render_template("video_downloader_home.html")
+
+@app.route("/get-video-info" , methods = ['POST'])
+def video_info():
+        yt = YouTube(request.form['url'])
+        session['videoname'] = yt.title
+        video_details = {
+                'title':yt.title,
+                'thumbnail' :yt.thumbnail_url,
+        }
+        return video_details, 200
+
+
+@app.route("/download-video" , methods = ['POST'])
+def download_video():
+        yt = YouTube(request.form['url'])
+        quality = request.form['quality']
+        stream = yt.streams.get_by_itag(quality)
+        print('download video')
+        stream.download(f"flask_app/static/videos")
+        print('download video')
+        return   
+
+        # return video
+
+
+
+        
+        
+
+@app.route("/delete-videos" , methods = ['POST'])
+def delete_videos():
+        os.remove(f'flask_app/static/uploads')
+        session.clear()
+        return 200
+                
+        
+        
+        
